1316c0f77fd50685220d820f2a1523374662cd5c
jetstream0 <113315327+downloadablecontent@users.noreply.github.com>
2023-12-09 22:29:19 +0000

    navbar and login styling

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { fetchPost } from "$lib/util/fetch.ts";
+
+  export let show_log_out: boolean = true;
+
+  const onLogout = async () => {
+    const res = await fetchPost("/api/user/logout", undefined)
+      .then(async (res: any) => {
+        console.log(res.status);
+        console.log(await res.text());
+
+        if (res.status >= 400) {
+          alert("failed, check console");
+        }
+      })
+  }
+</script>
+
+<div class="navbar w-full">
+  <div class="navbar-start">
+    <!--placeholder to make navbar-end work properly-->
+    <span> </span>
+  </div>
+  <div class="navbar-end">
+    {#if show_log_out}
+      <button on:click={onLogout} class="btn btn-error">logout</button>
+    {/if}
+  </div>
+</div>
+
+<style>
+  .navbar {
+    max-height: 10vh;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-import { fetchPost } from "$lib/util/fetch.ts";
+  import { fetchPost } from "$lib/util/fetch.ts";
+  import Navbar from "$lib/Navbar.svelte";
 
 let username: string = "";
 let name: string = "";
@@ -31,36 +32,28 @@ const onLogin = async () => {
       }
     })
 }
-
-const onLogout = async () => {
-  const res = await fetchPost("/api/user/logout", undefined)
-    .then(async res => {
-      console.log(res.status);
-      console.log(await res.text());
-
-      if (res.status >= 400) {
-        alert("failed, check console");
-      }
-    })
-}
 </script>
 
-<main class="h-full">
-  <div class="grid grid-flow-col grid-cols-1 lg:grid-cols-2 justify-center place-items-center h-5/6 pb-0.5">
+<!--assumes login page will not be shown while logged in (pls redirect)-->
+<Navbar show_log_out={false}/>
+
+<!-- yes I know navbar is not h-1/6 -->
+<main class="h-5/6">
+  <div class="grid grid-cols-1 lg:grid-cols-2 place-items-center h-full">
     <div>
       <h1 class="text-5xl lg:text-7xl">Bhargology</h1>
       <br>
     </div>
     <div>
-      <div>
-        <input bind:value={username} type="text" placeholder="username" class="input input-bordered"><br>
-        <input bind:value={name} type="text" placeholder="name" class="input input-bordered"><br>
-        <input bind:value={password} type="text" placeholder="password" class="input input-bordered"><br>
+      <div class="mb-2">
+        <input bind:value={username} type="text" placeholder="username" class="input input-bordered rounded-b-none focus:input-primary"><br>
+        <input bind:value={name} type="text" placeholder="name" class="input input-bordered rounded-none focus:input-primary"><br>
+        <input bind:value={password} type="text" placeholder="password" class="input input-bordered rounded-t-none focus:input-primary"><br>
       </div>
-
-      <button on:click={onCreate} class="btn btn-accent">create</button>
-      <button on:click={onLogin} class="btn btn-accent">login</button>
-      <button on:click={onLogout} class="btn btn-error">logout</button>
+      <div class="join">
+        <button on:click={onCreate} class="btn btn-accent join-item">create</button>
+        <button on:click={onLogin} class="btn btn-accent join-item">login</button>
+      </div>
     </div>
   </div>
 </main>
